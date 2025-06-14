0a8150bd3b4432aba1a56f826f899dbe6c55b510
christophthonhauser <c.thonhauser97@gmail.com>
2025-06-14 10:14:29 +0200

    add mobile navbar

---
+++ src/lib/NavMobile.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { Navigation } from '@skeletonlabs/skeleton-svelte';
+	// Icons
+	import IconFolder from '@lucide/svelte/icons/folder';
+	import IconImage from '@lucide/svelte/icons/image';
+	import IconMusic from '@lucide/svelte/icons/music';
+	import IconVideo from '@lucide/svelte/icons/video';
+
+	let value = $state('files');
+</script>
+
+<Navigation.Bar>
+	<Navigation.Tile label="Home" href="/"><IconFolder /></Navigation.Tile>
+	<Navigation.Tile label="Blog" href="/blog"><IconImage /></Navigation.Tile>
+	<Navigation.Tile label="Anleitung" href="/anleitung"><IconMusic /></Navigation.Tile>
+	<Navigation.Tile label="Impressum" href="/impressum"><IconVideo /></Navigation.Tile>
+</Navigation.Bar>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,19 @@
 <script lang="ts">
 	import '../app.css';
+	import NavDesktop from '$lib/NavDesktop.svelte';
+	import NavMobile from '$lib/NavMobile.svelte';
 
 	let { children } = $props();
-	import Nav from '$lib/Nav.svelte';
 </script>
 
-<div class="card grid min-h-screen w-full grid-cols-[auto_1fr]">
-	<Nav/>
+<div class="card grid min-h-screen w-full grid-rows-[1fr_auto] lg:grid-cols-[auto_1fr]">
+	<div class="hidden lg:block">
+		<NavDesktop />
+	</div>
 	<div class="p-4">
 		{@render children()}
 	</div>
+	<div class="lg:hidden">
+		<NavMobile />
+	</div>
 </div>
-
-
