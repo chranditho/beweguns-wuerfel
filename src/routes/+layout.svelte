a0d1bae8a07693abb408861e3c568cf564f17e86
christophthonhauser <c.thonhauser97@gmail.com>
2025-07-23 17:01:40 +0200

    further style page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,13 +7,13 @@
 </script>
 
 <div class="card grid min-h-screen w-full grid-rows-[1fr_auto] lg:grid-cols-[auto_1fr]">
-	<div class="hidden lg:block">
+	<div class="hidden lg:block h-screen sticky top-0">
 		<NavDesktop />
 	</div>
 	<div class="p-4">
 		{@render children()}
 	</div>
-	<div class="lg:hidden">
+	<div class="lg:hidden sticky bottom-0">
 		<NavMobile />
 	</div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,60 +1,58 @@
 <script>
+	import Cards from '$lib/cards/Cards.svelte';
+	import Hero from '$lib/hero/Hero.svelte';
+
+	const hauptprobleme = {
+		headerText: 'Wir lösen drei Hauptprobleme:',
+		cards:
+			[
+				{
+					title: '🎯 Hohe Zugangshürden zu inklusivem Sport',
+					description: 'Mobilität (immer irgendwo hinfahren zu müssen) und starre Strukturen schrecken viele ab. Unser spielerischer Ansatz macht Bewegung zugänglich.'
+				},
+				{
+					title: '💡 Mangel an spielerischer Bewegung',
+					description: 'Bewegung soll Spaß machen! Unser Würfel macht Bewegung zum Spiel und fördert so die Motivation.'
+				},
+				{
+					title: '🌱 Fehlende Nachhaltigkeit',
+					description: 'Viele Bewegungsangebote sind einmalig. Wir bilden Bewegungsbeauftragte aus, die die Idee weitergeben. Mit dem Würfel hat man auch eine Erinnerung an die Bewegung.'
+				}
+			]
+	};
+
+	const wasUnsAusmacht = {
+		headerText: 'Was uns ausmacht',
+		cards: [
+			{
+				title: '🎲 Spielerisch bewegen',
+				description: 'Der Bewegungswürfel macht Bewegung zum Spiel. Einfach würfeln und loslegen - so einfach kann Bewegung sein!'
+			},
+			{
+				title: '🤝 Partizipativ entwickeln',
+				description: 'Gemeinsam mit Menschen mit und ohne Behinderung entwickeln wir neue Spielformen und Bewegungsideen.'
+			},
+			{
+				title: '🌟 Nachhaltig wirken',
+				description: 'Durch die Ausbildung von Bewegungsbeauftragten schaffen wir langfristige Strukturen für inklusive Bewegung.'
+			}
+		]
+	};
 </script>
 
-<div class="space-y-4">
-	<h1 class="h1">Beweg-Uns-Würfel</h1>
-	<p class="h6">Inklusive Bewegung für alle - spielerisch barrierefrei</p>
+<main class="flex flex-col justify-between text-center gap-40">
+	<div class="space-y-2">
+		<h1 class="h1 text-primary-500">Beweg-Uns-Würfel</h1>
+		<p class="h6 text-secondary-400">Inklusive Bewegung für alle - spielerisch barrierefrei</p>
+	</div>
+
+	<Hero />
+
+	<Cards {...wasUnsAusmacht} />
+	<Cards {...hauptprobleme} />
+
 	<div>
-		Wir schaffen niederschwellige Zugänge zu Bewegung und Sport für Menschen mit und ohne Behinderung,
-		Durch unseren inklusiven Bewegungswürfel machen wir Bewegung im Alltag spielerisch und zugänglich.
+		<h2>Unser nächster Workshop</h2>
+		<p>Lerne den Bewegungswürfel kennen und werde Teil unserer inklusiven Bewegung.</p>
 	</div>
-	<div class="flex gap-2">
-		<button type="button" class="btn preset-filled-primary-500">Workshop-Termine</button>
-		<button type="button" class="btn preset-filled-secondary-500">Mehr erfahren</button>
-	</div>
-</div>
-
-<div class="space-y-8">
-	<h2 class="h2 text-center">Was uns ausmacht</h2>
-	<div class="flex flex-wrap gap-4">
-		<div class="card w-full max-w-md preset-filled-surface-100-900 p-4 text-center space-y-2">
-			<h3 class="h3">🎲 Spielerisch bewegen</h3>
-			<p>Der Bewegungswürfel macht Bewegung zum Spiel. Einfach würfeln und loslegen - so einfach kann Bewegung sein!</p>
-		</div>
-
-		<div class="card w-full max-w-md preset-filled-surface-100-900 p-4 text-center space-y-2">
-			<h3 class="h3">🤝 Partizipativ entwickeln</h3>
-			<p>Gemeinsam mit Menschen mit und ohne Behinderung entwickeln wir neue Spielformen und Bewegungsideen.</p>
-		</div>
-
-		<div class="card w-full max-w-md preset-filled-surface-100-900 p-4 text-center space-y-2">
-			<h3 class="h3">🌟 Nachhaltig wirken</h3>
-			<p>Durch die Ausbildung von Bewegungsbeauftragten schaffen wir langfristige Strukturen für inklusive Bewegung.</p>
-		</div>
-	</div>
-</div>
-
-<div class="space-y-8">
-	<h2 class="h2 text-center">Wir lösen drei Hauptprobleme:</h2>
-	<div class="flex flex-wrap gap-4">
-		<div class="card w-full max-w-md preset-filled-surface-100-900 p-4 text-center space-y-2">
-			<h3 class="h3">🎯 Hohe Zugangshürden zu inklusivem Sport</h3>
-			<p>Mobilität (immer irgendwo hinfahren zu müssen) und starre Strukturen schrecken viele ab. Unser spielerischer
-				Ansatz macht Bewegung zugänglich.</p>
-		</div>
-
-		<div class="card w-full max-w-md preset-filled-surface-100-900 p-4 text-center space-y-2">
-			<h3 class="h3">💡 Mangel an spielerischer Bewegung</h3>
-			<p>Bewegung soll Spaß machen! Unser Würfel macht Bewegung zum Spiel und fördert so die Motivation.</p>
-		</div>
-
-		<div class="card w-full max-w-md preset-filled-surface-100-900 p-4 text-center space-y-2">
-			<h3 class="h3">🌱 Fehlende Nachhaltigkeit</h3>
-			<p>Viele Bewegungsangebote sind einmalig. Wir bilden Bewegungsbeauftragte aus, die die Idee weitergeben. Mit dem
-				Würfel hat man auch eine Erinnerung an die Bewegung.</p>
-		</div>
-	</div>
-</div>
-
-<h2>Unser nächster Workshop</h2>
-<p>Lerne den Bewegungswürfel kennen und werde Teil unserer inklusiven Bewegung.</p>
+</main>
