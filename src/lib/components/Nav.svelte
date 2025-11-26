bb980422b5523f7a2503b5e0513513e407cf5445
christophthonhauser <c.thonhauser97@gmail.com>
2025-11-26 20:15:45 +0100

    add pages

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,12 +1,16 @@
 
 <nav>
 	<div class="flex justify-center">
+		<a href="/aktuell" class="hover:bg-gray-200 p-2 transition-all">AKTUELL</a>
+		<div class="bg-gray-900 w-0.5"></div>
 		<a href="/workshop" class="hover:bg-gray-200 p-2 transition-all">WORKSHOP</a>
 		<div class="bg-gray-900 w-0.5"></div>
-		<a href="/impressum" class="hover:bg-gray-200 p-2 transition-all">IMPRESSUM</a>
+		<a href="/das-spiel" class="hover:bg-gray-200 p-2 transition-all">DAS SPIEL</a>
+		<div class="bg-gray-900 w-0.5"></div>
+		<a href="/ueber-uns" class="hover:bg-gray-200 p-2 transition-all">ÜBER UNS</a>
 		<div class="bg-gray-900 w-0.5"></div>
 		<a href="/kontakt" class="hover:bg-gray-200 p-2 transition-all">KONTAKT</a>
 		<div class="bg-gray-900 w-0.5"></div>
-		<a href="/ueber-uns" class="hover:bg-gray-200 p-2 transition-all">ÜBER UNS</a>
+		<a href="/impressum" class="hover:bg-gray-200 p-2 transition-all">IMPRESSUM</a>
 	</div>
 </nav>
--- src/routes/das-spiel/+page.svelte
+++ src/routes/das-spiel/+page.svelte
@@ -1,5 +1,5 @@
 <script>
 </script>
 <div class="items-center max-w-screen-lg mx-auto space-y-10">
-	<h1 class="text-3xl font-bold mb-6">Workshop</h1>
+	<h1 class="text-3xl font-bold mb-6">Das Spiel</h1>
 </div>
