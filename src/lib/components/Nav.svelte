b3dc942d8d9d1027cccef2277025c3d5846cab70
christophthonhauser <c.thonhauser97@gmail.com>
2025-11-29 13:18:35 +0100

    make nav responsive

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,16 +1,8 @@
-
-<nav>
-	<div class="flex justify-center">
-		<a href="/aktuell" class="hover:bg-gray-200 p-2 transition-all">AKTUELL</a>
-		<div class="bg-gray-900 w-0.5"></div>
-		<a href="/workshop" class="hover:bg-gray-200 p-2 transition-all">WORKSHOP</a>
-		<div class="bg-gray-900 w-0.5"></div>
-		<a href="/das-spiel" class="hover:bg-gray-200 p-2 transition-all">DAS SPIEL</a>
-		<div class="bg-gray-900 w-0.5"></div>
-		<a href="/ueber-uns" class="hover:bg-gray-200 p-2 transition-all">ÜBER UNS</a>
-		<div class="bg-gray-900 w-0.5"></div>
-		<a href="/kontakt" class="hover:bg-gray-200 p-2 transition-all">KONTAKT</a>
-		<div class="bg-gray-900 w-0.5"></div>
-		<a href="/impressum" class="hover:bg-gray-200 p-2 transition-all">IMPRESSUM</a>
-	</div>
+<nav class="grid grid-cols-3 lg:divide-x gap-2 lg:gap-0 md:grid-cols-6 justify-center items-center">
+		<a href="/aktuell" class="hover:bg-gray-200 p-2 transition-all  text-center">AKTUELL</a>
+		<a href="/workshop" class="hover:bg-gray-200 p-2 transition-all  text-center">WORKSHOP</a>
+		<a href="/das-spiel" class="hover:bg-gray-200 p-2 transition-all  text-center">DAS SPIEL</a>
+		<a href="/ueber-uns" class="hover:bg-gray-200 p-2 transition-all  text-center">ÜBER UNS</a>
+		<a href="/kontakt" class="hover:bg-gray-200 p-2 transition-all  text-center">KONTAKT</a>
+		<a href="/impressum" class="hover:bg-gray-200 p-2 transition-all  text-center">IMPRESSUM</a>
 </nav>
